d49ac1346f07e3a1cedbcc952511fd3f6509bb17
csmurillo <csmurillo00@gmail.com>
2023-09-02 17:00:03 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const http = require('http');
+const express = require('express');
+const mongoose = require('mongoose');
+
+const cors = require('cors');
+const app = express();
+
+const authRoutes = require('./routes/auth');
+const productRoutes = require('./routes/product');
+const server = http.createServer(app);
+
+app.use(express.json());
+app.use(cors({origin:'*'}));
+
+mongoose.connect(process.env.DATABASE)
+    .then(()=>{console.log('db connected');})
+    .catch(()=>{console.log('error db');});
+
+app.use('/api',authRoutes);
+app.use('/api',productRoutes);
+
+const port = process.env.PORT || 3004;
+
+server.listen(port,()=>{
+    console.log(`port is ${port}`);
+});
