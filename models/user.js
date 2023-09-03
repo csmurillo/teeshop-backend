2bdfdabb96d7dc2ca788aef7408dae87186d02ba
csmurillo <csmurillo00@gmail.com>
2023-09-02 17:10:03 -0700

    model user added

---
+++ models/user.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+const userSchema = mongoose.Schema({
+    firstName:{
+        type:String,
+        required:true
+    },
+    lastName:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    password:{
+        type:String,
+        required:true
+    },
+    orderHistory:{
+        type:Array,
+        default:[]
+    }
+});
+
+module.exports = mongoose.model('User',userSchema);
+
+
